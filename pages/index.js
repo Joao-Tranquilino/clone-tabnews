e2608c4e5c1d368cdbc31b9e0f6053230956af07
João Pedro Tranquilino Barbosa <111088845+Joao-Tranquilino@users.noreply.github.com>
2026-05-12 20:55:51 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+function Home() {
+    return (
+        <><h1>Renata eu amo você. Se você me ama, da uma risadinha!</h1><Image src="/" alt="Imagem" width={500} height={300} /></>
+    );
+}
+
+export default Home;
