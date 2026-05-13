2e72c32c105daf421112d2e8d0d21a956f0ea441
João Pedro Tranquilino Barbosa <111088845+Joao-Tranquilino@users.noreply.github.com>
2026-05-13 19:53:00 +0000

    Feita alterações

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 function Home() {
     return (
-        <><h1>Renata eu amo você. Se você me ama, da uma risadinha!</h1><Image src="/" alt="Imagem" width={500} height={300} /></>
+        <><h1>Renata eu amo você. Se você me ama, da uma risadinha!</h1></>
     );
 }
 
